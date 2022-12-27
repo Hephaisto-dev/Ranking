e8da4511ece28f16f620739a019ff434842cc3b9
Hephaisto-dev <hephaistopl@gmail.com>
2022-12-27 18:41:31 +0100

    Add table creation automation
    Took 11 minutes

--- src/main/java/fr/hephaisto/ranking/sql/Database.java
+++ src/main/java/fr/hephaisto/ranking/sql/Database.java
@@ -1,9 +1,9 @@
 package fr.hephaisto.ranking.sql;
 
 import fr.hephaisto.ranking.Ranking;
+import org.bukkit.configuration.ConfigurationSection;
 
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.logging.Logger;
@@ -11,6 +11,7 @@ import java.util.logging.Logger;
 public class Database {
     private final DbConnection dbConnection;
     private String table_name;
+    private ConfigurationSection columns_section;
 
     public Database(Ranking plugin) {
         dbConnection = new DbConnection(new DbCredentials(
@@ -20,6 +21,7 @@ public class Database {
                 plugin.getConfig().getString("database.database"),
                 plugin.getConfig().getInt("database.port")));
         table_name = plugin.getConfig().getString("database.table");
+        columns_section = plugin.getConfig().getConfigurationSection("database.columns");
     }
 
     public boolean init() {
@@ -29,12 +31,16 @@ public class Database {
                 Logger.getLogger("Ranking").severe("Database connection failed, please check your config.yml");
                 return false;
             }
-            // Check if table exists using connection metadata
-            DatabaseMetaData metaData = connection.getMetaData();
-            if (!metaData.getTables(null, null, table_name, null).next()) {
-                Logger.getLogger("Ranking").severe("Table " + table_name + " doesn't exists");
-                return false;
-            }
+            // Create table if not exists
+            final String[] sql_request = {"CREATE TABLE IF NOT EXISTS rankhebdo(id int(255) PRIMARY KEY AUTO_INCREMENT," +
+                    " faction varchar(255), activity int(255), management float(53), economy float(53)," +
+                    " military float(53), technology float(53), created_at varchar(255), updated_at varchar(255)," +
+                    " build float(53), total float(53), bourse float(53), member int(53));"
+                            .replace("rankhebdo", table_name)};
+            columns_section.getKeys(false).forEach(column ->
+                    sql_request[0] = sql_request[0].replace(column, columns_section.getString(column)));
+            PreparedStatement query = connection.prepareStatement(sql_request[0]);
+            query.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
