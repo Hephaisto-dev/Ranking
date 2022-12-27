99ee1d2aea6d9bf3f9c33c35852444019554a4e0
Hephaisto-dev <hephaistopl@gmail.com>
2022-12-27 22:03:47 +0100

    Complete EconomyCalculator without db
    Took 16 minutes

--- src/main/java/fr/hephaisto/ranking/calculators/EconomyCalculator.java
+++ src/main/java/fr/hephaisto/ranking/calculators/EconomyCalculator.java
@@ -1,7 +1,9 @@
 package fr.hephaisto.ranking.calculators;
 
 import com.massivecraft.factions.entity.Faction;
+import com.massivecraft.massivecore.money.Money;
 import fr.hephaisto.ranking.Ranking;
+import world.nations.Core;
 
 public class EconomyCalculator extends AbstractCalculator{
     public EconomyCalculator(Ranking plugin) {
@@ -9,6 +11,19 @@ public class EconomyCalculator extends AbstractCalculator{
     }
 
     public double compute(Faction faction) {
+        double points = 0;
+        double ratioPointsMoney = plugin.getConfig().getDouble("criteria.economy.ratio-points-money");
+        double maxPointsMoney = plugin.getConfig().getDouble("criteria.economy.max-points-money");
+        double ratioPointsWeekProfit = plugin.getConfig().getDouble("criteria.economy.ratio-points-week-profit");
+        double maxPointsWeekProfit = plugin.getConfig().getDouble("criteria.economy.max-points-week-profit");
+        double balance = Core.getPlugin().getEconomyManager().getBalance(faction.getName());
+        points += Math.min(Money.get(faction) * ratioPointsMoney, maxPointsMoney);
+        points += Math.min((balance - getLastBalance(faction)) * ratioPointsWeekProfit, maxPointsWeekProfit);
+        return points;
+    }
+
+    private double getLastBalance(Faction faction) {
+        //TODO get last week profit from db
         return 0;
     }
 }
